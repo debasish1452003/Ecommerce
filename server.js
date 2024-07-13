cd68045393f063d08174b90076b97313f90db143
debasish1452003 <debasishrana1452003@gmail.com>
2024-07-14 00:01:53 +0530

    vercel commit

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-// import path from "path";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 // configure env
 dotenv.config();
@@ -18,12 +20,15 @@ connectDB();
 // rest object
 const app = express();
 
+// get __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // middlewares
-// not used in production, helps to inform us about the request by users really helpful during the development stage
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -31,25 +36,23 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 // rest api
-// --------------88888888---------------------
 app.get("/", (req, res) => {
   res.send({
-    message: "Welcome to ecommerce app ",
+    message: "Welcome to ecommerce app",
   });
 });
 
-// app.use("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // PORT
-// PORTY is in .env it is imported
-const PORT = process.env.PORTY;
+const PORT = process.env.PORT || 8080;
 
 // run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+    `Server running in ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
       .white
   );
 });
